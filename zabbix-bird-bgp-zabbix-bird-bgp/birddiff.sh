4aced6c38afd39797c4549d0bbb1230baf4d465a
N-Mi <nicolas.pichon42@free.fr>
2020-12-15 19:07:46 +0100

    Fix so that it only greps needed conf file
    Otherwise, it picks also bird.conf~, bird.conf.bak, bird.conf.dpkg-dist...

--- zabbix-bird-bgp-zabbix-bird-bgp/birddiff.sh
+++ zabbix-bird-bgp-zabbix-bird-bgp/birddiff.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #Scan bird conf files and get configured bgp session and show difference between birdc output
-CONFIG=$(grep -r "^protocol bgp" /etc 2>/dev/null | wc -l)
+CONFIG=$(grep "^protocol bgp" /etc/bird/bird.conf 2>/dev/null | wc -l)
 BIRDC=$(sudo birdc show proto | grep -cw BGP)
 
 test "$CONFIG" = "$BIRDC" && echo '0' || echo '1'
